36fe54cc64de4a68e103e6b06149f91c8362259e
Alia <aliachip@gmail.com>
2023-07-20 18:13:45 -0700

    adding the completed HW for postgreSQL

---
+++ employeeSQL/employees_db.sql
@@ -0,0 +1,39 @@
+-- CREATE TABLE titles(
+-- title_id CHAR(10),
+-- title VARCHAR (66),
+-- PRIMARY KEY (title_id)
+-- );
+
+-- CREATE TABLE departments(
+-- dept_no CHAR(10),
+-- dept_name VARCHAR(66),
+-- PRIMARY KEY (dept_no)
+-- );
+
+-- CREATE TABLE employees(
+-- emp_no INT,
+-- emp_title_id CHAR(10),
+-- birth_date DATE,
+-- first_name VARCHAR (66),
+-- last_name VARCHAR (66),
+-- sex CHAR(1),
+-- hire_date DATE,
+-- PRIMARY KEY (emp_no)
+-- );
+
+-- CREATE TABLE dept_emp(
+-- emp_no INT,
+-- dept_no CHAR(10)
+-- );
+
+-- CREATE TABLE dept_manager(
+-- dept_no CHAR(10),
+-- emp_no INT,
+-- PRIMARY KEY (emp_no)
+-- );
+
+-- CREATE TABLE salaries(
+-- emp_no INT,
+-- salary INT,
+-- PRIMARY KEY (emp_no)
+-- );
